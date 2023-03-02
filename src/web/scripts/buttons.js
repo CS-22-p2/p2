cf97048e3cefe575603cc6088f2e1229c2a18ad3
Johan-Ravn <jran22@student.aau.dk>
2023-03-02 13:46:45 +0100

    Added some changes

--- src/web/scripts/buttons.js
+++ src/web/scripts/buttons.js
@@ -8,3 +8,16 @@ function underline_current(param) {
     }
     document.links[current].className = 'active';
 }
+
+const appear = document.querySelector('.fade-in-animation'); 
+const cb = function(entries){
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+        entry.target.classList.add('inview');
+        }else{
+        entry.target.classList.remove('inview');
+        }
+});
+}
+const io = new IntersectionObserver(cb);
+io.observe(appear);
