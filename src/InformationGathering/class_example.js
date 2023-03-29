c8d7fad1dbed8a8965cf6c3db97821b35f30f005
Adomas Ciplys <JZ44VN@student.aau.dk>
2023-03-29 12:48:00 +0200

    Our Event Class

---
+++ src/InformationGathering/class_example.js
@@ -0,0 +1,14 @@
+class Event {
+    constructor(orgName, orgType,contactInfo, link, eventTitle, participants, location, duration, isPrivate, description) {
+      this.orgName = orgName;
+      this.orgType = orgType;
+      this.contactInfo = contactInfo;
+      this.link = link;
+      this.eventTitle = eventTitle;
+      this.participants = participants;
+      this.location = location;
+      this.duration = duration;
+      this.isPrivate = isPrivate;
+      this.description = description; 
+    }
+  }
