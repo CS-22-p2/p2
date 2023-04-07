a7b91b39fd3ebf69ecf6b9ff334b4c58c61f126f
Johan-Ravn <jran22@student.aau.dk>
2023-04-07 19:32:51 +0200

    Implementing Jest unit testing

--- src/data-process/relevancy-score.js
+++ src/data-process/relevancy-score.js
@@ -1,18 +1,18 @@
 // Input from webcrawler will look like
-class Event {
-    constructor(orgName, orgType,contactInfo, link, eventTitle, participants, location, duration, isPrivate, description) {
-      this.orgName = orgName;
-      this.orgType = orgType;
-      this.contactInfo = contactInfo;
-      this.link = link;
-      this.eventTitle = eventTitle;
-      this.participants = participants;
-      this.location = location;
-      this.duration = duration;
-      this.isPrivate = isPrivate;
-      this.description = description; 
-    }
-}
+// class Event {
+//     constructor(orgName, orgType,contactInfo, link, eventTitle, participants, location, duration, isPrivate, description) {
+//       this.orgName = orgName;
+//       this.orgType = orgType;
+//       this.contactInfo = contactInfo;
+//       this.link = link;
+//       this.eventTitle = eventTitle;
+//       this.participants = participants;
+//       this.location = location;
+//       this.duration = duration;
+//       this.isPrivate = isPrivate;
+//       this.description = description; 
+//     }
+// }
 
 
 // When event is happening relative to current time
