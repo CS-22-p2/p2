51cefca83b9baf403580881dcb1b2914430f9404
Adomas Ciplys <JZ44VN@student.aau.dk>
2023-04-13 10:13:34 +0200

    Login Working

--- src/InformationGathering/url_processor.js
+++ src/InformationGathering/url_processor.js
@@ -5,8 +5,9 @@ import { scrapeOrgData, scrape} from "./org_crawler.js";
 const popUp_click = "div.x1iorvi4.xdl72j9";
 
 
-//const orgData = await scrapeOrgData(scrape);
-await loginFB();
+const orgData = await scrapeOrgData(scrape);
+await logEvents(orgData);
+let browser = await loginFB();
 
 async function loginFB(){
 
@@ -19,11 +20,16 @@ async function loginFB(){
     await page.goto("https://www.facebook.com/", {
         waitUntil: "networkidle2"
     });
-    await page.type('#email', 'testing testing', {delay: 30});
-    await page.type('#pass', 'testing testing', {delay: 30});
-    await page.click('#u_0_5_xS');
-
-
+    await page.click('button._42ft._4jy0._9xo7._4jy3._4jy1.selected._51sy');
+    await page.type('#email', 'williamscrapius@gmail.com', {delay: 30});
+    await page.type('#pass', 'cs23sw202', {delay: 30});
+    await page.click('button._42ft._4jy0._6lth._4jy6._4jy1.selected._51sy', {delay: 30});
+    await page.waitForNetworkIdle(30);
+    await page.goto('https://www.facebook.com/IdaPaaAau', {
+        waitUntil: 'networkidle2',
+        delay: 30
+    })
+    return browser;
 }
 
 async function accessEventsPage(orgData)
