eb5b939047378ad94ffaf1999b0f50e13b6cc9db
Johan-Ravn <jran22@student.aau.dk>
2023-04-14 21:36:23 +0200

    added unit test with jest

--- src/data-process/relevancy-score.test.js
+++ src/data-process/relevancy-score.test.js
@@ -1,2 +1,10 @@
-import RelevancyScore from './relevancy-score';
 
+const { describe } = require('node:test');
+const testing = require('./relevancy-score');
+
+test('Should remove numbers, trailing whitespace, and lowercase the word', () => {
+  expect(testing.format_string("Selmalagerløfsvej 12")).toBe("selmalagerløfsvej");
+  expect(testing.format_string(" Selmalagerløfsvej 12")).toBe("selmalagerløfsvej");
+  expect(testing.format_string(" Selmalagerløfsvej 12 ")).toBe("selmalagerløfsvej");
+  expect(testing.format_string(" Selma lagerløfsvej 12 ")).toBe("selma lagerløfsvej");
+});
