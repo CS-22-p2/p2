377d5dd8fce95126533bdba2d1534d0f07630044
Theis Jensen <Theis@bmpnj.dk>
2023-04-21 13:15:24 +0200

    LoginPage put request

--- src/HTTP-Server/public/loginPage.js
+++ src/HTTP-Server/public/loginPage.js
@@ -1,6 +1,10 @@
+import { json } from "stream/consumers";
+
 let button = document.getElementsByName("login");
 console.log(button);
 button[0].addEventListener("click", submit);
+
+const url = '/path/to/resource';
 
 function submit(){
     let values = "";
@@ -8,4 +12,19 @@ function submit(){
     values += "Password = " + document.getElementsByName("password")[0].value + "\n";
     alert(values);
     console.log(values);
+
+    let data = {Email: document.getElementsByName("mail")[0].value,
+                         Password: document.getElementsByName("password")[0].value};
+
+    fetch(url, {
+        method: "put",
+        headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+    }).then(response => {
+        console.log('PUT request successful');
+    }).catch(error => {
+        console.error('Error sending PUT request:', error);
+    });
 }
