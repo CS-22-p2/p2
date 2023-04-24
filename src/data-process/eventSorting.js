531d0ef4a788d486b7f571cb0f31da380b2b6670
Adomas Ciplys <JZ44VN@student.aau.dk>
2023-04-24 11:40:09 +0200

    Sorting Working Fixed

--- src/data-process/eventSorting.js
+++ src/data-process/eventSorting.js
@@ -3,6 +3,8 @@ import {date_conversion_formatting} from "./event-insertion.js";
 import { type } from "os";
 import { test } from "node:test";
 
+
+//--------------JUST USED FOR TESTING----------------------
 const testDates = [
     "WEDNESDAY, 26 APRIL 2023 AT 18:30 UTC+02",
     "17 JUN AT 16:00 – 18 JUN AT 02:00 UTC+02",
@@ -23,7 +25,7 @@ const testDates = [
     "WEDNESDAY, 17 MAY 2023 FROM 17:00-01:00 UTC+02",
     "FRIDAY, 5 MAY 2023 FROM 16:00-22:00 UTC+02",
     "MONDAY, 24 APRIL 2023 AT 16:00 UTC+02",
-    "WEDNESDAY, 26 APRIL 2023 AT 16:00 UTC+02",
+    "trolo trolo tolllol trolloolo trlolo",
     "MONDAY, 15 MAY 2023 AT 16:00 UTC+02",
     "TUESDAY, 25 APRIL 2023 FROM 16:30-18:00 UTC+02",
     "TUESDAY, 16 MAY 2023 FROM 17:00-20:00 UTC+02",
@@ -44,11 +46,15 @@ function setTestDates(testDates)
 
     return processedDateArray;
 }
+//-----------------------------------------------------------------
 
+//Used to sort events by date or by relevancy score
+//Works "in place" so we do not need to return array from function
 function InsertionSort(array)
 {
-    invalidDateChecker(array);
-    
+    //Checks if the dates are valid and corrects array if invalid dates occur
+    let invalidIndexes = invalidDateChecker(array); 
+
     //Insertion Sort --- See CLRS ch. 2.1
     for(let j = 1; j < array.length; j++)
     {
@@ -61,26 +67,30 @@ function InsertionSort(array)
         }
         array[i + 1]=key;
     }
-
+    return invalidIndexes;
 }
 
 function invalidDateChecker(array){
-    
+    let invalidIndexes = [];
+
     //Check if an invalid date is passed into the array
-    for(let element of array)
+    for(let element in array)
     {
-        if(element.toString() === "Invalid Date") //If an invalid date is passed it is removed form the array
+        if(array[element].toString() === "Invalid Date") //If an invalid date is passed it is removed form the array
         {
-            array.splice(element,1);
+            invalidIndexes.push(parseInt(element)); //Store invalid index for later use
+            array.splice(element, 1)
         }
     }
+    return invalidIndexes;
 }
 
 //-------Executed Commands-----
 let eventDates = setTestDates(testDates);
 console.log(eventDates);
-InsertionSort(eventDates);
+let check = InsertionSort(eventDates);
 console.log(eventDates);
+console.log(check);
 let testNumbers = [100,10,5,17,20,9,0,-3,28,3,1,77];
 console.log(testNumbers);
 InsertionSort(testNumbers);
